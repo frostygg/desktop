e7dd5503706bb33adf463556e4b56d04d857e0c7
daniel11420 <kiziolek06@gmail.com>
2019-12-25 00:54:54 +0100

    Try to maybe fix auto-updater

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, dialog} = require('electron');
 const isDev = require('electron-is-dev');
 if (isDev) { } else {
   const {autoUpdater} = require('electron');
@@ -68,6 +68,29 @@ app.on('activate', function () {
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) createWindow();
 });
+
+if (isDev) { } else {
+  autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+    const dialogOpts = {
+      type: 'info',
+      buttons: ['Restart', 'Later'],
+      title: 'Application Update',
+      message: process.platform === 'win32' ? releaseNotes : releaseName,
+      detail: 'A new version has been downloaded. Restart the application to apply the updates.'
+    }
+  
+    dialog.showMessageBox(dialogOpts).then((returnValue) => {
+      if (returnValue.response === 0) autoUpdater.quitAndInstall();
+    });
+  });
+
+  autoUpdater.on('error', message => {
+    console.error('There was a problem updating the application');
+    console.error(message);
+  });
+}
+
+
 
 setInterval(clearCache, 1000*60*5);
 //if (isDev === false) {
