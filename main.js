1facdb4240d6ad870269c4c0e9e9231838ea07cb
daniel11420 <kiziolek06@gmail.com>
2019-12-24 16:42:24 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,63 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+
+let pluginName
+switch (process.platform) {
+  case 'win32':
+    pluginName = 'flash/pepflashplayer64_32_0_0_303.dll'
+    break
+  case 'darwin':
+    pluginName = 'flash/PepperFlashPlayer.plugin'
+    break
+  case 'linux':
+    pluginName = 'flash/libpepflashplayer.so'
+    break
+}
+app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname, pluginName));
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function clearCache() {
+  mainWindow.webContents.session.clearCache();
+}
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    title: "Club Penguin Back is loading...",
+    icon: __dirname + '/favicon.ico',
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      plugins: true
+    }
+  });
+
+  mainWindow.setMenu(null);
+  mainWindow.loadURL('http://play.cpback.club/desktop_game.html');
+  clearCache();
+
+  //mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function () {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) createWindow();
+});
+
+setInterval(clearCache, 1000*60*5);
---
+++ preload.js
@@ -0,0 +1,19 @@
+/*// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+})
+*/
+
+function redirect(){window.location.href = 'http://play.cpback.club/desktop_game.html';}
+window.addEventListener('DOMContentLoaded', () => {
+  var url = window.location.href;
+    if(url.includes('play.cpback.club') == false){redirect();}
+  });
